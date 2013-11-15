ee6236e6b8584f5da10301e278815a392b318576
Jon Palmer <jmpalmer@mitre.org>
2013-11-15 14:52:35 -0500

    started on simple routes

---
+++ config/express.js
@@ -0,0 +1,12 @@
+// create express object to be returned
+var express = require('express');
+var provider = require('./mongoose.js')
+
+var app = module.exports = express.createServer();
+
+
+// setup routes
+
+
+//return express object
+return express;
