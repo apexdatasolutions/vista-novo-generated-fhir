88c959936163ecc338aeed6a74b32801b0bddb31
Jon Palmer <jmpalmer@mitre.org>
2013-11-17 17:52:06 -0500

    added 'findAll' method to return all model names in app/models. commented out require for models because some have errors

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,8 +3,19 @@ mongoose.connect('mongodb://localhost/fhir');
 
 var fs = require('fs');
 var models_path = __dirname + '/../app/models';
-fs.readdirSync(models_path).forEach(function (file) {
+var models = fs.readdirSync(models_path);
+models.forEach(function (file) {
   if (~file.indexOf('.js')) {
-    require(models_path + '/' + file);
+    //console.log('Trying to require %s',file);
+    //require(models_path + '/' + file);
   }
 });
+
+exports.findAll = function(callback) {
+  var temp = []
+  models.forEach(function (file) {
+    //console.log('Trying to require %s',file);
+    temp.push(file.split(".")[0]);
+  });
+  callback(null,temp);
+}
