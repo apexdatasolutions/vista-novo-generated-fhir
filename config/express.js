0c04469e4841b47cc5e7d673f909e3627253d4eb
Jon Palmer <jmpalmer@mitre.org>
2013-11-17 18:02:01 -0500

    added some app configuration for views, rendering engine, public folder, etc

--- config/express.js
+++ config/express.js
@@ -5,6 +5,17 @@ var provider = require('./mongoose.js')
 // create app object and setup as exported member of module
 var app = express();
 
+// configure the app
+app.configure(function(){
+  app.set('views', __dirname + '/app/views');
+  app.set('view engine', 'eco');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
 
 // setup routes
 app.get('/', function(req, res){
