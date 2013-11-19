b096afb831be295aa0ef40843413ae43f3d5fc1e
Jon Palmer <jmpalmer@mitre.org>
2013-11-19 10:28:00 -0500

    finally got config for eco rendering engine working. also, started playing with routes

--- config/express.js
+++ config/express.js
@@ -1,32 +1,41 @@
 // create express object to be returned
-var express = require('express');
-var provider = require('./mongoose.js')
+var express  = require('express');
+var provider = require('./mongoose.js');
+var cons      = require('consolidate')
 
 // create app object and setup as exported member of module
 var app = express();
 
-// configure the app
-app.configure(function(){
-  app.set('views', __dirname + '/app/views');
-  app.set('view engine', 'eco');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
+//configure app
+// assign the swig engine to .html files
+app.engine('eco', cons.eco);
+
+// set .html as the default extension 
+app.set('view engine', 'eco');
+app.set('views', __dirname + '/../app/views');
+
+
+
 
 
 // setup routes
+
+// root url
 app.get('/', function(req, res){
     provider.findAll( function(error,collection){
-        res.render('index.eco', { 
-            locals: {
+        res.render('index', { 
                 title: 'Model Index',
                 models: collection
-            }
         });
     })
+});
+
+//index for model
+app.get('/:model', function(req,res){
+   res.render('test', {
+       model: req.params.model
+   });
+    
 });
 
 exports.app = app;
