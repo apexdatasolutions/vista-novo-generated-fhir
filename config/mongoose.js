7d76bc6dfab976235e1e3dd6c76ae977581a58c8
Jon Palmer <jmpalmer@mitre.org>
2013-11-20 22:06:08 -0500

    got adverse reaction model loading for now and nothing else

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,12 +4,19 @@ mongoose.connect('mongodb://localhost/fhir');
 var fs = require('fs');
 var models_path = __dirname + '/../app/models';
 var models = fs.readdirSync(models_path);
-models.forEach(function (file) {
-  if (~file.indexOf('.js')) {
-    //console.log('Trying to require %s',file);
-    //require(models_path + '/' + file);
-  }
-});
+
+// NOTE: some models seem to be broken. once fixed, the following code will work
+
+// models.forEach(function (file) {
+//   if (~file.indexOf('.js')) {
+//     console.log('Trying to require %s',file);
+//     require(models_path + '/' + file);
+//   }
+// });
+
+// For now, import model for testing
+require(models_path + '/adversereaction.js')
+require(models_path + '/resource_history.js')
 
 exports.findAll = function(callback) {
   var temp = []
