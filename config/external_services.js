07ac28178f054ed8b8bae5ff4e678dd265e03d85
Dave Hill <dwhill@mitre.org>
2014-10-24 17:00:03 -0400

    Updated test stub server location to match configuration of secure RESTful pilot.  Passes access token to underlying service.

--- config/external_services.js
+++ config/external_services.js
@@ -2,25 +2,29 @@
 // that will be called to pull in information to be translated
 // into FHIR
 exports.observations = {
-  url: "http://localhost:3001/admin/observation/",
+  url: "http://localhost:3001/test-stub/api/observations/",
+  // url: "http://localhost:3001/api/observations/",
   username: 'andy@mitre.org',
   password: 'splatter'
 };
 
 exports.patients = {
-  url: "http://localhost:3001/admin/patient/",
+  url: "http://localhost:3001/test-stub/api/patients/",
+  // url: "http://localhost:3001/api/patients/",
   username: 'andy@mitre.org',
   password: 'splatter'
 };
 
 exports.medications = {
-  url: "http://localhost:3001/admin/medication/",
+  url: "http://localhost:3001/test-stub/api/medications/",
+  // url: "http://localhost:3001/api/medications/",
   username: 'andy@mitre.org',
   password: 'splatter'
 };
 
 exports.conditions = {
-  url: "http://localhost:3001/admin/condition/",
+  url: "http://localhost:3001/test-stub/api/conditions/",
+  // url: "http://localhost:3001/api/conditions/",
   username: 'andy@mitre.org',
   password: 'splatter'
 };
