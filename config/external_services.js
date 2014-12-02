a7958e72f8a6f85b5c1e89005ec8f6b74ac467b2
Dave Hill <dwhill@mitre.org>
2014-12-02 11:16:27 -0500

    Removed 'test-stub' from URIs in external_services.js

--- config/external_services.js
+++ config/external_services.js
@@ -2,29 +2,25 @@
 // that will be called to pull in information to be translated
 // into FHIR
 exports.observations = {
-  url: "http://localhost:3001/test-stub/api/observations/",
-  // url: "http://localhost:3001/api/observations/",
+  url: "http://localhost:3001/api/observations/",
   username: 'andy@mitre.org',
   password: 'splatter'
 };
 
 exports.patients = {
-  url: "http://localhost:3001/test-stub/api/patients/",
-  // url: "http://localhost:3001/api/patients/",
+  url: "http://localhost:3001/api/patients/",
   username: 'andy@mitre.org',
   password: 'splatter'
 };
 
 exports.medications = {
-  url: "http://localhost:3001/test-stub/api/medications/",
-  // url: "http://localhost:3001/api/medications/",
+  url: "http://localhost:3001/api/medications/",
   username: 'andy@mitre.org',
   password: 'splatter'
 };
 
 exports.conditions = {
-  url: "http://localhost:3001/test-stub/api/conditions/",
-  // url: "http://localhost:3001/api/conditions/",
+  url: "http://localhost:3001/api/conditions/",
   username: 'andy@mitre.org',
   password: 'splatter'
 };
